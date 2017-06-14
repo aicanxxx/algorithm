f797267ff4497dec9b1e300eb57e95bc7d2880d3
aicanxxx <985568626@qq.com>
2017-06-14 20:47:08 +0800

    commit

--- 牛客网/magicBracelet.js
+++ 牛客网/magicBracelet.js
@@ -2,18 +2,18 @@
  * Created by Administrator on 2017/5/27 0027.
  */
 /*
-* 魔力手环
+ * 魔力手环
  小易拥有一个拥有魔力的手环上面有n个数字(构成一个环),当这个魔力手环每次使用魔力的时候就会发生一种奇特的变化：每个数字会变成自己跟后面一个数字的和(最后一个数字的后面一个数字是第一个),一旦某个位置的数字大于等于100就马上对100取模(比如某个位置变为103,就会自动变为3).现在给出这个魔力手环的构成，请你计算出使用k次魔力之后魔力手环的状态。
-输入描述:
+ 输入描述:
  输入数据包括两行：
  第一行为两个整数n(2 ≤ n ≤ 50)和k(1 ≤ k ≤ 2000000000),以空格分隔
  第二行为魔力手环初始的n个数，以空格分隔。范围都在0至99.
-输出描述:
+ 输出描述:
  输出魔力手环使用k次之后的状态，以空格分隔，行末无空格。
-输入例子:
+ 输入例子:
  3 2
  1 2 3
-输出例子:
+ 输出例子:
  8 9 7
  */
 process.stdin.resume();//回复输入流
@@ -38,37 +38,12 @@ process.stdin.on('end', function () {//end触发
 function readLine() {
     return input_stdin_array[input_currentline++];//读取每一行的数据
 }
-
-/*function main(){
-    var num=readLine().split(' ');
-    var arr=readLine().split(' ');
-    var n=num[0];
-    var k=num[1];
-    var temp=0;
-    arr=arr.map(function(item){
-        return +item;
-    })
-    for(var i=0;i<k;i++){
-        temp=arr[0];
-        for(var j=0;j<n;j++){
-            if(j==n-1){
-                arr[j] += temp;
-            }else{
-                arr[j]+=arr[j+1];
-            }
-            if(arr[j]>=100){
-                arr[j] %=100;
-            }
-        }
-
-    }
-    console.log(arr.join(' '));
-}*/
-function mulOrder(x,y,n,c){//n阶矩阵的乘法与取余x:ixk,y:kxj
-    var mul=[];//mul为全0矩阵mul:i*j
+function mulOrder(x,y,n,c){//n阶矩阵的乘法与取余，x:nxn,y:nxn
+    var mul=[];//mul为全0矩阵,mul:n*n
     for(var i=0;i<n;i++){
         mul[i]=[];
         for(var j=0;j<n;j++){
+            mul[i][j]=0;
             for(var k=0;k<n;k++){
                 mul[i][j] +=parseInt(x[i][k])*parseInt(y[k][j]);
             }
@@ -78,8 +53,9 @@ function mulOrder(x,y,n,c){//n阶矩阵的乘法与取余x:ixk,y:kxj
     return mul;
 }
 function oneOrder(x,y,n,c){//一阶矩阵的乘法与取余x:1xn,y:nxn
-    var mul=[];//mul为全0矩阵1xn
+    var mul=[];//mul为全0矩阵,mul:1xn
     for(var i=0;i<n;i++){
+        mul[i]=0;
         for(var j=0;j<n;j++){
             mul[i] +=parseInt(x[j])*parseInt(y[j][i]);
         }
@@ -87,39 +63,25 @@ function oneOrder(x,y,n,c){//一阶矩阵的乘法与取余x:1xn,y:nxn
     }
     return mul;
 }
-/*function operation(matrix,k,n){
-    var res=[];//res为单位阵
-    for(var i=0;i<n;i++){
-        res[i]=[];
-        res[i][i]=1;
-    }
-    while(k){
-        if(k&1){
-            res=multiply(res,matrix,n);
-        }
-        k>>1;
-        matrix=multiply(matrix,matrix,n)
-    }
-    return res;
-}*/
 function main(){
     var num=readLine().split(' ');
     var arr=readLine().split(' ');
     var n=parseInt(num[0]);
     var k=parseInt(num[1]);
-    var matrix=[],res=[];
-    /*for(var i=0;i<n;i++){
-        res[i]=[];
-        res[0][i] += arr[i];
-    }*/
-    //创建矩阵B和单位阵
-    for(var i=0;i<n;i++){
+    var matrix=[];
+    //创建矩阵B
+    for(var i=0;i<n;i++){//对矩阵B赋值
         matrix[i]=[];
+        for(var j=0;j<n;j++){
+            matrix[i][j]=0;
+        }
+    }
+    for(var i=0;i<n;i++){
         matrix[i][i]=1;
         if(i==n-1){
-            matrix[i][0]=1;
+            matrix[0][i]=1;
         }else{
-            matrix[i][i+1]=1;
+            matrix[i+1][i]=1;
         }
     }
     while(k){
@@ -129,8 +91,5 @@ function main(){
         k>>=1;
         matrix=mulOrder(matrix,matrix,n,100);
     }
-    /*for(var i=0;i<n;i++){
-        arr[i]=res[0][i]
-    }*/
     console.log(arr.join(' '));
 }
